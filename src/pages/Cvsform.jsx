537e902f262c2a252dd6134eac393d34b50ad7bd
TS22082 <ts22082@gmail.com>
2018-10-15 18:20:37 -0700

    csv file contents shown in console.

--- src/pages/Cvsform.jsx
+++ src/pages/Cvsform.jsx
@@ -8,12 +8,21 @@ class Cvsfrom extends Component {
     this.handleSubmitedFiles = this.handleSubmitedFiles.bind(this);
     this.showUploadedFiles = this.showUploadedFiles.bind(this);
   }
+
   handleSubmitedFiles = files => {
     // console.log(files.base64);
     // console.log(files.fileList):
     let namesArray = [];
     const filesObject = files.fileList;
     this.setState({ submittedFiles: filesObject });
+    const reader = new FileReader();
+
+    reader.readAsText(filesObject[0]);
+
+    reader.onload = () => {
+      console.log(reader.result);
+    };
+
     for (let i = 0; i < this.state.submittedFiles.length; i++) {
       namesArray.push(this.state.submittedFiles[i].name);
       this.setState({ namesArray });
@@ -40,17 +49,15 @@ class Cvsfrom extends Component {
             <div className="card">
               <div className="card-header bg-dark text-light">
                 <h4 className="card-title mb-0">
-                  Welcome To The CSV form page!
+                  Welcome To The CSV submit page!
                 </h4>
               </div>
               <div className="card-body">
-                <h3 className="card-title">
-                  Use this page to upload CSV files or zip folders
-                </h3>
+                <h3 className="card-title">Use this page to upload CSV file</h3>
                 <ReactFileReader
-                  fileTypes={[".csv", ".zip"]}
+                  fileTypes={[".csv"]}
                   base64={true}
-                  multipleFiles={true}
+                  multipleFiles={false}
                   handleFiles={this.handleSubmitedFiles}
                 >
                   <button className="btn">Upload</button>
