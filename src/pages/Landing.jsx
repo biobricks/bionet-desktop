4ad0d5e041b3cb37f9105608885984b2be275cc9
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-10-16 18:39:12 -0700

    remove broken video link

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -75,7 +75,7 @@ class Landing extends Component {
                   <h4 className="card-title mb-0 text-capitalize">Hello {this.props.currentUser.username}!</h4>
                 </div>
                 <div className="card-body">
-                  {(this.state.labs.length === 0 && this.state.labsJoined.length === 0) ? (
+                  {(this.state.labsJoined.length === 0) ? (
                     <p className="card-text">
                       Congratulations on your new BioNet installation!<br/> 
                       We can see that you have yet to configure or join your lab(s), let's do that.<br/><br/>
@@ -107,16 +107,6 @@ class Landing extends Component {
                   <p className="card-text">
                     Keep track of your stuff, find what you need, and share as you like. The bionet supports true asynchronous, peer-peer inventory management and sharing — all your inventory information is controlled locally by you. You decide if others can see what you wish to share. All BioNet software and associated materials are free to use.
                   </p>
-                  <h2 className="card-title">How Does It Work?</h2>
-                  <iframe 
-                    title="BioNet"
-                    width="100%" 
-                    height="400" 
-                    className="mt-1"
-                    src="https://www.youtube.com/embed/t29-RGggSU8?ecver=1" 
-                    frameBorder="0" 
-                    allow="autoplay; encrypted-media" 
-                    allowFullScreen></iframe>
                 </div>
               </div>
             )}
